bd7d9adebb7216ead721670a7f65fa38283e7873
singlemice <singlemice@gmail.com>
2013-02-18 12:43:58 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+$db_host="10.61.13.52";
+$port="3306";
+$db_name="star";
+$db_user="star";
+$db_pwd="star";
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="img/style.css" rel="stylesheet" type="text/css" />
+
+<title>登录</title>
+<?php 
+require_once(realpath(dirname(__FILE__)."/config/session.php"));
+if(!isset($_SESSION["login_status"]))
+{
+	echo "<script>window.location =\"login.html\";</script>";
+}
+else {
+	echo "<script>window.location =\"main.php\";</script>";
+}
+?>
+</head>
+<body>
+
+</body>
+</html>
